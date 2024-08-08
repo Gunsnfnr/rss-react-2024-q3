d2f361c0dc85bbf23dbfe947cc5cad25b533d01c
Gunsnfnr <a.j.p.010203@gmail.com>
2024-08-08 22:10:21 +0300

    refactor: refactor characters components and styles

--- src/components/CharacterCard/CharacterCard.tsx
+++ src/components/CharacterCard/CharacterCard.tsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import { Character } from '../Main/Main';
 import { useContext, useEffect, useState } from 'react';
 import style from './CharacterCard.module.css';
@@ -6,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { addCard, removeCard } from '../../store/cardsSlice';
 import { RootState } from '../../store';
 import { ThemeContext } from '../../context/themeContext';
+import { useRouter } from 'next/router';
 
 interface Props {
   character: Character;
@@ -13,10 +13,11 @@ interface Props {
 
 const CharacterCard = (props: Props) => {
   const url = props.character.url;
-  const idOfCharacter = url.split('people/')[1].slice(0, -1);
+  const id = url.split('people/')[1].slice(0, -1);
   const [isChecked, setIsChecked] = useState(false);
   const dispatch = useDispatch();
   const selectedCards = useSelector((state: RootState) => state.charactersCards.selectedCards);
+  const router = useRouter();
 
   useEffect(() => {
     const isInSelected = selectedCards.some((card) => {
@@ -39,22 +40,28 @@ const CharacterCard = (props: Props) => {
     }
   };
 
+  const handleCharacterCardClick = () => {
+    if (typeof router.query.search === 'string' && typeof router.query.page === 'string')
+      router.push(`/?search=${router.query.search}&page=${router.query.page}&id=${id}`).catch(() => {});
+  };
+
   return (
-    <Link to={`card/${idOfCharacter}`} className={style.link}>
-      <div className={theme === 'light' ? style.star_wars_character_light : style.star_wars_character}>
-        <input
-          className={style.checkbox}
-          type="checkbox"
-          checked={isChecked}
-          onClick={(event) => {
-            event.stopPropagation();
-          }}
-          onChange={handleCheckboxChange}
-          title="Click to choose"
-        />
-        <div className={style.name}>{props.character.name}</div>
-      </div>
-    </Link>
+    <div
+      className={theme === 'light' ? style.star_wars_character_light : style.star_wars_character}
+      onClick={handleCharacterCardClick}
+    >
+      <input
+        className={style.checkbox}
+        type="checkbox"
+        checked={isChecked}
+        onClick={(event) => {
+          event.stopPropagation();
+        }}
+        onChange={handleCheckboxChange}
+        title="Click to choose"
+      />
+      <div className={style.name}>{props.character.name}</div>
+    </div>
   );
 };
 
--- src/components/CharacterDetails/CharacterDetails.tsx
+++ src/components/CharacterDetails/CharacterDetails.tsx
@@ -1,38 +1,51 @@
-import { Link, useNavigate, useParams } from 'react-router-dom';
 import style from './CharacterDetails.module.css';
-import { swCharactersApi } from '../../store/apiSlice';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { ThemeContext } from '../../context/themeContext';
+import useLoading from '../../hooks/useLoading';
+import { useRouter } from 'next/router';
+import { Character } from '../Main/Main';
 
-const CharacterDetails = () => {
-  const { id } = useParams<string>();
-  const { data: character, isFetching } = swCharactersApi.useGetCharacterByIdQuery(id);
+const CharacterDetails = ({ characterData }: { characterData: Character | null }) => {
   const { theme } = useContext(ThemeContext);
-  const navigate = useNavigate();
+  const [isLoading] = useLoading();
+  const router = useRouter();
+  const [isUnmounting, setIsUnmounting] = useState(false);
+
+  useEffect(() => {
+    return () => {
+      setIsUnmounting(false);
+    };
+  }, [isUnmounting]);
+
+  const handleToMainClick = () => {
+    setIsUnmounting(true);
+    if (typeof router.query.search === 'string' && typeof router.query.page === 'string')
+      router.push(`/?search=${router.query.search}&page=${router.query.page}`).catch(() => {});
+  };
 
   return (
     <>
-      {isFetching && (
+      {isLoading && router.query.id && !isUnmounting && (
         <div className={theme === 'light' ? style.loading_card_light : style.loading_card}>Loading...</div>
       )}
-      {!isFetching && character && (
+      {!isLoading && characterData && (
         <>
           <div className={theme === 'light' ? style.character_card_light : style.character_card}>
             <div>
-              <div className={style.name}>{character.name}</div>
+              <div className={style.name}>{characterData.name}</div>
               <div>
-                <div>Height: {character.height} cm</div>
-                <div>Mass: {character.mass} kg</div>
-                <div>Birth year: {character.birth_year}</div>
-                <div>Eye color: {character.eye_color}</div>
-                <div>Skin color: {character.skin_color}</div>
+                <div>Height: {characterData.height} cm</div>
+                <div>Mass: {characterData.mass} kg</div>
+                <div>Birth year: {characterData.birth_year}</div>
+                <div>Eye color: {characterData.eye_color}</div>
+                <div>Skin color: {characterData.skin_color}</div>
               </div>
-              <Link to="/">
-                <button className={style.close}>Close card</button>
-              </Link>
+              <button className={style.close} onClick={handleToMainClick}>
+                Close card
+              </button>
             </div>
           </div>
-          <div className={style.character_card_backdrop} onClick={() => navigate('/')} aria-label="Close"></div>
+          <div className={style.character_card_backdrop} onClick={handleToMainClick} aria-label="Close"></div>
         </>
       )}
     </>
