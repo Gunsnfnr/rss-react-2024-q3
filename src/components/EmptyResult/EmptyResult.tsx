b03db9e68044685403955636472c4aee54a03a1c
Gunsnfnr <a.j.p.010203@gmail.com>
2024-08-08 22:21:17 +0300

    refactor: refactor EmptyResult component

--- src/components/EmptyResult/EmptyResult.tsx
+++ src/components/EmptyResult/EmptyResult.tsx
@@ -1,26 +1,20 @@
 import { useContext } from 'react';
 import style from './EmptyResult.module.css';
 import { ThemeContext } from '../../context/themeContext';
+import { useRouter } from 'next/router';
 
-interface EmptyResultProps {
-  searchQuery: string;
-}
-
-const EmptyResult = (props: EmptyResultProps) => {
-  const getLastSearch = (): string => {
-    return localStorage.getItem('gunsnfnr.swQuery') ?? '';
-  };
+const EmptyResult = () => {
+  const router = useRouter();
   const { theme } = useContext(ThemeContext);
 
   return (
-    <div className={theme === 'dark' ? style.empty : style.empty_light}>
+    <section className={theme === 'dark' ? style.empty : style.empty_light}>
       <div>
         Nothing was found for&nbsp;the&nbsp;search&nbsp;term&nbsp;&quot;
-        {props.searchQuery ? props.searchQuery : getLastSearch()}
-        &quot;.
+        {router.query.search}&quot;.
       </div>
       <div>Don&apos;t forget, we are looking for the Star Wars characters o_0</div>
-    </div>
+    </section>
   );
 };
 
